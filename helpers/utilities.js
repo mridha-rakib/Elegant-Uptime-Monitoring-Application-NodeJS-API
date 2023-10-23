3661a179f1511f3d8ca49a634b537aee5aec81c0
RAKIB <rakibmahmudmridha@gmail.com>
2023-10-23 18:08:35 +0600

    files added and modified

--- helpers/utilities.js
+++ helpers/utilities.js
@@ -9,9 +9,8 @@
 
 // module scaffolding
 const crypto = require("crypto");
-
-const utilities = {};
 const environments = require("./environments");
+const utilities = {};
 
 // parse JSON string to Object
 utilities.parseJSON = (jsonString) => {
@@ -24,6 +23,37 @@ utilities.parseJSON = (jsonString) => {
   }
 
   return output;
+};
+
+utilities.hash = (str) => {
+  if (typeof str === "string" && str.length > 0) {
+    console.log(environments, process.env.NODE_ENV);
+    const hash = crypto
+      .createHmac("sha256", environments.secretKey)
+      .update(str)
+      .digest("hex");
+    return hash;
+  }
+  return false;
+};
+
+// create random string
+utilities.createRandomString = (strlength) => {
+  let length = strlength;
+  length = typeof strlength === "number" && strlength > 0 ? strlength : false;
+
+  if (length) {
+    const possiblecharacters = "abcdefghijklmnopqrstuvwxyz1234567890";
+    let output = "";
+    for (let i = 1; i <= length; i += 1) {
+      const randomCharacter = possiblecharacters.charAt(
+        Math.floor(Math.random() * possiblecharacters.length)
+      );
+      output += randomCharacter;
+    }
+    return output;
+  }
+  return false;
 };
 
 module.exports = utilities;
